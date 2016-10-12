f9e0c739d6da1622a9df28306e5d26f45e824bc9
Lluís Revilla <lluis.revilla@gmail.com>
2016-10-12 11:44:09 +0200

    Adding roxygen2 documentation to exportNetworkToVisANT and exportNetworkToCytoscape

--- R/exportFunctions.R
+++ R/exportFunctions.R
@@ -1,5 +1,44 @@
 # Functions for exporting networks to various network visualization software
-
+#' @name exportNetworkToVisANT
+#' @rdname exportNetworkToVisANT
+#' @title Export network data in format readable by VisANT
+#' @description
+#' Export network data in a format readable and displayable by the VisANT
+#' software.
+#' @param adjMat adjacency matrix of the network to be exported.
+#' @param file character string specifying the file name of the file in which
+#' the data should be written. If not given, no file will be created. The file
+#' is in a plain text format.
+#' @param weighted logical: should the exported network be weighted?
+#' @param threshold adjacency threshold for including edges in the output.
+#' @param maxNConnections maximum number of exported adjacency edges. This can
+#' be used as another filter on the exported edges.
+#' @param probeToGene optional specification of a conversion between probe names
+#' (that label columns and rows of adjacency) and gene names (that should label
+#' nodes in the output).
+#' @details
+#' The adjacency matrix is checked for validity. The entries can be negative,
+#' however. The adjacency matrix is expected to also have valid names or
+#' dimnames[[2]] that represent the probe names of the corresponding edges.
+#'
+#' Whether the output is a weighted network or not, only edges whose (absolute
+#' value of) adjacency are above threshold will be included in the output. If
+#' maxNConnections is given, at most maxNConnections will be included in the
+#' output.
+#'
+#' If probeToGene is given, it is expected to have two columns, the first one
+#' corresponding to the probe names, the second to their corresponding gene
+#' names that will be used in the output.
+#' @return
+#' A data frame containing the network information suitable as input to VisANT.
+#' The same data frame is also written into a file specified by file, if given.
+#' @author
+#' Peter Langfelder
+#' @references
+#' VisANT software is available from \link{http://visant.bu.edu/}
+#' @seealso
+#' \code{\link{exportNetworkToCytoscape}}
+#' @export
 exportNetworkToVisANT = function(
   adjMat,
   file = NULL,
@@ -16,7 +55,7 @@ exportNetworkToVisANT = function(
   if (!is.null(probeToGene))
   {
     probes2genes = match(probes, probeToGene[,1])
-    if (sum(is.na(probes2genes)) > 0) 
+    if (sum(is.na(probes2genes)) > 0)
       stop("Error translating probe names to gene names: some probe names could not be translated.")
     probes = probeToGene[probes2genes, 2]
   }
@@ -48,6 +87,28 @@ exportNetworkToVisANT = function(
   invisible(visAntData)
 }
 
+#' @name exportNetworkToCytoscape
+#' @rdname exportNetworkToCytoscape
+#' @title Export network to Cytoscape
+#' @description
+#' This function exports a network in edge and node list files in a format
+#' suitable for importing to Cytoscape.
+#' @inheritParams  exportNetworkToVisANT
+#' @details
+#' If the corresponding file names are supplied, the edge and node data is
+#' written to the appropriate files. The edge and node data is also returned as
+#' return value (see below).
+#' @return
+#' A list with the following componens:
+#' \describe{
+#' \item{egdeData}{a data frame containing the edge data, with one row per edge}
+#' \item{nodeData}{a data frame containing the node data, with one row per node}
+#' }
+#' @author
+#' Peter Langfelder
+#' @seealso
+#' \code{\link{exportNetworkToVisANT}}
+#' @export
 exportNetworkToCytoscape = function(
   adjMat,
   edgeFile = NULL,
@@ -64,7 +125,7 @@ exportNetworkToCytoscape = function(
   nRow = nrow(adjMat)
   checkAdjMat(adjMat, min = -1, max = 1)
   if (is.null(nodeNames)) nodeNames = dimnames(adjMat)[[1]]
-  if (is.null(nodeNames)) 
+  if (is.null(nodeNames))
     stop("Cannot determine node names: nodeNames is NULL and adjMat has no dimnames.")
   rowMat = matrix(c(1:nRow), nRow, nRow, byrow = TRUE)
   colMat = matrix(c(1:nRow), nRow, nRow)
@@ -104,14 +165,10 @@ exportNetworkToCytoscape = function(
   if (!is.null(edgeFile))
     write.table(edgeData, file = edgeFile, quote = FALSE, row.names = FALSE, col.names = includeColNames,
                 sep = "\t")
-  
+
   if (!is.null(nodeFile))
     write.table(nodeData, file = nodeFile, quote = FALSE, row.names = FALSE, col.names = includeColNames,
                 sep = "\t")
 
   list(edgeData = edgeData, nodeData = nodeData)
 }
-  
-
-
-
