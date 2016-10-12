4b17ba8e06c1a1824d17b8f9a8e179d9ebb3ee21
Lluís Revilla <lluis.revilla@gmail.com>
2016-10-12 13:25:30 +0200

    Documenting matrixToNetwork with roxygen2 syntax

--- R/Functions-fromSimilarity.R
+++ R/Functions-fromSimilarity.R
@@ -1,5 +1,45 @@
 # Functions to perform WGCNA from similarity input.
-
+#' @rdname matrixToNetwork
+#' @name matrixToNetwork
+#' @title Construct a network from a matrix
+#' @description
+#' Constructs a network
+#' @param mat matrix to be turned into a network. Must be square.
+#' @param symmetrizeMethod method for symmetrizing the matrix. The method will
+#' be applied to each component of mat and its transpose.
+#' @param signed logical: should the resulting network be signed? Unsigned
+#' networks are constructed from abs(mat).
+#' @param min minimum allowed value for mat. If NULL, the actual attained
+#' minimum of mat will be used. Missing data are ignored. Values below min are
+#' truncated to min.
+#' @param max maximum allowed value for mat. If NULL, the actual attained
+#' maximum of mat will be used. Missing data are ignored. Values below max are
+#' truncated to max.
+#' @param power the soft-thresholding power.
+#' @param the value of the entries on the diagonal in the result. This is usally
+#' 1 but some applications may require a zero (or even NA) diagonal.
+#' @details
+#' If signed is FALSE, the matrix mat is first converted to its absolute value.
+#'
+#' This function then symmetrizes the matrix using the symmetrizeMethod
+#' component-wise on mat and t(mat) (i.e., the transpose of mat).
+#'
+#' In the next step, the symmetrized matrix is linearly scaled to the interval
+#' [0,1] using either min and max (each either supplied or determined from the
+#' matrix). Values outside of the [min, max] range are truncated to min or max.
+#'
+#' Lastly, the adjacency is calculated by rasing the matrix to power. The
+#' diagonal of the result is set to diagEntry. Note that most WGCNA functions
+#' expect the diagonal of an adjacency matrix to be 1.
+#' @return
+#' The adjacency matrix that encodes the network
+#' @author
+#' Peter Langfelder
+#' @seealso
+#' \code{\link{adjacency}} for calculation of a correlation network (adjacency)
+#' from a numeric matrix such as expression data, and adjacency.fromSimilarity
+#' for simpler calculation of a network from a symmetric similarity matrix.
+#' @export
 matrixToNetwork <- function(mat,
                             symmetrizeMethod = c("average", "min", "max"),
                             signed = TRUE,
