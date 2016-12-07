a8423e162fc1d5d88c280a546224ee541bbfb3e0
Lluís Revilla <lluis.revilla@gmail.com>
2016-12-07 23:36:40 +0100

    Update documentation of accuracyMeasures

--- tests/testthat/test_accuracyMeasures.R
+++ tests/testthat/test_accuracyMeasures.R
@@ -12,7 +12,10 @@ test_that("accuracyMeasures in table format", {
     test2 <- accuracyMeasures(predictedOutcome, trueOutcome)
     expect_equal(nrow(test1), 14L)
     expect_equal(ncol(test1), 2L)
-    expect_true(all.equal(colnames(test), c("Measure", "Value")))
+    expect_equal(nrow(test2), 14L)
+    expect_equal(ncol(test2), 2L)
+    expect_true(all.equal(colnames(test1), c("Measure", "Value")))
+    expect_true(all.equal(colnames(test2), c("Measure", "Value")))
 
     # Test if they are both the same matrix
     expect_true(all.equal(dim(test1), dim(test2)))
