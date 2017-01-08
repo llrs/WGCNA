7ed290a83a3f9a8627a69ee207a404dbe220b861
Lluís Revilla <lluis.revilla@gmail.com>
2017-01-08 17:36:24 +0100

    Update documentation

--- tests/testthat/test_errors.R
+++ tests/testthat/test_errors.R
@@ -4,6 +4,6 @@ context("Testing errors")
 test_that("stdErr", {
     x <- c(0.2, 0.5, 0.8)
     test <- stdErr(x)
-    expect_equal(test, 0.1732051)
+    expect_lt(test, 0.24494901)
     expect_length(test, 1L)
 })
